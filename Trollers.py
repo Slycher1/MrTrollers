02c544744b3cd6ea6dea7d31f3b7a3538df58fc9
Slycher <34550177+Slycher1@users.noreply.github.com>
2021-06-22 17:30:06 +0300

    Add files via upload

---
+++ Trollers.py
@@ -0,0 +1,45 @@
+#####################Installing Libraries#####################
+import os
+try:
+    import nmap, colored, sockets  
+except:
+    print("Installing libraries")
+    os.system("pip install colored")
+    os.system("pip install sockets")
+    os.system("pip install python-nmap")
+    try:
+        os.system("sudo apt install git")
+    except:
+        os.system("Windows Support coming soon, This tool Only works in linux for now ):")
+    os.system("git clone git@bitbucket.org:xael/python-nmap.git")
+    os.system("pip install python-nmap")
+####################Rights And Fun####################
+print("""
+        ============================
+        =      My rights           =
+        =    twitter:@S75XD        =
+        =    twitter:@i_slyc       =
+        = twitter:@F14Commander    =
+        = twitter:@alhassanAlharb7 =
+        ============================     
+""")
+
+
+
+print("""
+i_slyc: hi lmao.
+MrXD: stfu...
+F14Commander: Bro Make your own Tools.... Scripter kiddie, sAudI arAbIa Is tHe bEsT!1!1!!! \n \n 
+Checking for Required Files....
+""")
+##########################################
+try:
+    os.system('cd Resources')
+except:
+    print("Couldn't Find Files.....")
+    print("\n \n Downloading files...")
+    #os.system("")
+
+
+
+    
